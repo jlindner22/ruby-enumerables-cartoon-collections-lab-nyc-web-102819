cb1ee97d887e51693e2e576be43e3062f08cf69f
Jen Lindner <jenlindner22@gmail.com>
2019-10-21 20:59:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,7 +24,7 @@ end
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-array.find_index{ |food| food == [cheese_types.include]} 
+array.find_index{ |food| food == [cheese_types.include?]} 
 end
 
 # find_index(value) → int or nil
