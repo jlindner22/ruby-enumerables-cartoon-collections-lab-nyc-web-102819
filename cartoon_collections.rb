d0920ebeb9e1c3ea2dd2c518be74cc55088c978d
Jen Lindner <jenlindner22@gmail.com>
2019-10-21 20:54:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,6 +28,16 @@ array.select.first{ |food| food == cheese_types}
 
 end
 
+find_index(value) → int or nilclick to toggle source
+find_index { |obj| block } → int or nil
+find_index → an_enumerator
+Compares each entry in enum with value or passes to block. Returns the index for the first for which the evaluated value is non-false. If no object matches, returns nil
+
+If neither block nor argument is given, an enumerator is returned instead.
+
+(1..10).find_index  { |i| i % 5 == 0 and i % 7 == 0 }  #=> nil
+(1..100).find_index { |i| i % 5 == 0 and i % 7 == 0 }  #=> 34
+(1..100).find_index(50)                                #=> 49
 
 # def reduce_to_all_true(source_array)
 #   i = 0 
